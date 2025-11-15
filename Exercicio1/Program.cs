4e99936df68925e29e2c9f3ff7b39c46287a35dc
mariosergio2025 <mariosergio.geraldo@gmail.com>
2025-11-15 16:46:20 -0300

    Primeiro exerciocio da lista 2

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Exercicio1
+﻿using System.Globalization;
+
+namespace Exercicio1
 {
+    //Faça um programa que leia quanto dinheiro tem na carteira e converta o valor em dolar
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            decimal cotacaoDolar = 5.15m;
+            Console.Write("Sr. usuário qual o valor em R$ você tem na carteira? ");
+            decimal dinheiroCarteira = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("Com a cotação de 1 dolar para "+cotacaoDolar.ToString("C", CultureInfo.CreateSpecificCulture("pt-br")));
+            Console.WriteLine(" e você tendo "+dinheiroCarteira.ToString("C", CultureInfo.CreateSpecificCulture("pt-br")) +" na carteira");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            decimal convertidoEmDollar = dinheiroCarteira / cotacaoDolar;
+            Console.WriteLine("O equivalente em dolar que possui na carteira são: " + convertidoEmDollar.ToString("C", CultureInfo.CreateSpecificCulture("en-us")));
         }
     }
 }
