f8f7dfb8349081be738fa82b1442ed9056391263
mariosergio2025 <mariosergio.geraldo@gmail.com>
2025-11-15 20:28:21 -0300

    Exercicio 2 da Lista

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Exercicio2
+{   // Programa Calculo da Area a ser Pintada e Quantidade Tinta
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 65; //Tamanho tela
+            Console.WriteLine("Calculo de Quantidade de Tinta necessaria para Pintura!");
+            Linha(t);
+            Console.WriteLine();
+            Console.Write("Sr. Usuário informa qual a altura da parede em metros? ");
+            float alturaParede = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Sr. Usuário informa qual a largura da parede em metros? ");
+            float larguraParede = Convert.ToSingle(Console.ReadLine());
+
+            float areaParede = CalArea(alturaParede, larguraParede);
+            Linha(t);
+            Console.WriteLine($"A parede tem {areaParede} m²");
+            const float rendimento = 0.5f;
+            Console.WriteLine($"O rendimento da tinta é {rendimento} m²/litro");
+            Linha(t);
+            float calTinta=CalTinta(areaParede, rendimento);
+            Console.WriteLine($"Precisa de {calTinta} litros de tinta");
+
+        }
+
+
+
+        public static float CalArea(float altura, float largura)
+        {
+            float calculoArea = altura * largura;
+            return calculoArea;
+        }
+        public static float CalTinta(float area, float rendimento)
+        {
+            float calTinta = area/rendimento;
+            return calTinta;
+        }
+
+        public static void Linha(int numeroCaracter)
+        {
+            for (int i=1;i<numeroCaracter-1;++i)
+            {
+                Console.Write("-");
+
+            }
+            Console.WriteLine("-");
+        }
+
+    }
+}
