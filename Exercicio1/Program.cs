e4d003447442306d359da8cbde0f8f69350a5037
mariosergio2025 <mariosergio.geraldo@gmail.com>
2025-11-18 17:25:39 -0300

    Atualização e mudança Exercicio 1 para Exercicio 0

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization;
 
-namespace Exercicio1
+namespace Exercicio0
 {
     //Faça um programa que leia quanto dinheiro tem na carteira e converta o valor em dolar
     internal class Program
@@ -10,15 +10,24 @@ namespace Exercicio1
             decimal cotacaoDolar = 5.15m;
             Console.Write("Sr. usuário qual o valor em R$ você tem na carteira? ");
             decimal dinheiroCarteira = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("-----------------------------------------------------------------------");
             Console.WriteLine("");
-            Console.WriteLine("");
+            
             Console.Write("Com a cotação de 1 dolar para "+cotacaoDolar.ToString("C", CultureInfo.CreateSpecificCulture("pt-br")));
             Console.WriteLine(" e você tendo "+dinheiroCarteira.ToString("C", CultureInfo.CreateSpecificCulture("pt-br")) +" na carteira");
             Console.WriteLine("");
-            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------------------------------");
             decimal convertidoEmDollar = dinheiroCarteira / cotacaoDolar;
             Console.WriteLine("O equivalente em dolar que possui na carteira são: " + convertidoEmDollar.ToString("C", CultureInfo.CreateSpecificCulture("en-us")));
+            Console.WriteLine("");
+            int emNotas= (int)Math.Floor(convertidoEmDollar);
+            Console.WriteLine("Mas considerando que a casa de cabio não trabalho com moedas de outros paises");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.Write($"Entao voce podera ter {emNotas.ToString("c", CultureInfo.CreateSpecificCulture("en-us"))}");
+            decimal sobraEmReal = (convertidoEmDollar - emNotas) * cotacaoDolar;
+            Console.WriteLine($" e tem sobra {sobraEmReal.ToString("C",CultureInfo.CreateSpecificCulture("pt-BR"))} sem trocar");
+            Console.WriteLine("");
+            Console.WriteLine("");
         }
     }
 }
