2d0df218bd3b90197ebbc01d01f31f613c9d6473
mariosergio2025 <mariosergio.geraldo@gmail.com>
2025-11-19 22:18:18 -0300

    Atualização exercicio 8 e numeração

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ptBR = new System.Globalization.CultureInfo("pt-BR");
+            Console.WriteLine("Digite o Valor do Saque: !");
+            var entrada = Console.ReadLine();
+            var valor = Convert.ToInt32(entrada);
+
+            //Calculando o numero de notas de 200
+            var qtdNotas200 = valor / 200;
+            valor = valor -(qtdNotas200/200);
+            if (qtdNotas200 > 0) { Console.WriteLine($"Entregando {qtdNotas200} de R$ 200,00 - Faltam {valor.ToString("C", ptBR)} reais do valor original do saque .."); }
+
+            //Calculando o numero de notas 100
+            var qtdNotas100 = valor / 100;
+            valor = valor % 100;
+            Console.WriteLine($"Entregando {qtdNotas100} de R$ 100,00 - Faltam {valor.ToString("C", ptBR)} reais do valor original do saque ..");
+
+            //Calculando o numero de notas 50
+            var qtdNotas50 = valor / 50;
+            valor = valor % 50;
+            Console.WriteLine($"Entregando {qtdNotas50} de R$ 50,00 - Faltam {valor.ToString("C", ptBR)} reais do valor original do saque ..");
+
+            //Calculando o numero de notas 20
+            var qtdNotas20 = valor / 20;
+            valor = valor % 20;
+            Console.WriteLine($"Entregando {qtdNotas20} de R$ 20,00 - Faltam {valor.ToString("C", ptBR)} reais do valor original do saque ..");
+
+            //Calculando o numero de notas 10
+            var qtdNotas10 = valor / 10;
+            valor = valor % 10;
+            Console.WriteLine($"Entregando {qtdNotas10} de R$ 10,00 - Faltam {valor.ToString("C", ptBR)} reais do valor original do saque ..");
+
+            //Calculando o numero de notas 5
+            var qtdNotas5 = valor / 5;
+            valor = valor % 5;
+            Console.WriteLine($"Entregando {qtdNotas5} de R$ 20,00 - Faltam {valor.ToString("C", ptBR)} reais do valor original do saque ..");
+
+            //Calculando o numero de notas 2
+            var qtdNotas2 = valor / 2;
+            valor = valor % 2;
+            Console.WriteLine($"Entregando {qtdNotas2} de R$ 2,00 - Faltam {valor.ToString("C", ptBR)} reais do valor original do saque ..");
+
+            Console.WriteLine(($"Entregando 1 de 1")
+        }
+    }
+}
--- Exercicio8/Program.cs
+++ Exercicio8/Program.cs
@@ -1,11 +1,10 @@
-﻿namespace Exercicio8
+﻿using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace Exercicio8
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Menu();
-        }
         // Variaveis iniciais
         public static int col = 35; // tamanho da linha
         // se vai logia do preenchido
@@ -20,11 +19,24 @@
         public static float aumento3 = 1.03f;
         public static float aumenta3a10 = 1.125f;
         public static float aumenta10eacima = 1.20f;
+        public static float novosalario = 0.00f;
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        public static void Linha(int col)
+        {
+            for (int i = 0; i < col - 1; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("-");
+        }
+
+       
 
         public static void Menu()
         {
-
-
             Console.Clear();
             Linha(col);
             Console.WriteLine("--------- MENU --------");
@@ -38,13 +50,14 @@
             Console.WriteLine("    5 - Zeras preenchimentos");
             Console.WriteLine("    6 - Sair");
             Linha(col);
-            int opcao = Convert.ToInt32(Console.ReadLine());
+            
+            string opcao = Console.ReadLine();
 
-            switch (opcao)
+            switch (Convert.ToInt32(opcao))
             {
-                case 1: TipoDeCarro(); break;
-                case 2: Dias(); break;
-                case 3: Distancia(); break;
+                case 1: NomeFunciorio(); break;
+                case 2: SalarioFuncionario(); break;
+                case 3: TempoEmpresa(); break;
                 case 4: Calculo(); break;
                 case 5: ZeraPreenchimento(); break;
                 case 6: break;
@@ -52,14 +65,95 @@
             }
 
         }
-        public static void Linha(int col)
+        public static void NomeFunciorio()
         {
-            for (int i = 0; i < col - 1; i++)
-            {
-                Console.Write("-");
-            }
-            Console.WriteLine("-");
+            Console.Clear();
+            Linha(col);
+            Console.Write("Digite o nome completo do funcionario : ");
+            nome = Console.ReadLine();
+            Linha(col);
+            Console.WriteLine("Nome do funcionario cadastrado...");
+            Linha(col);
+            preenchidoNome = true;
+            Console.WriteLine();
+            Console.WriteLine("<Precione uma tecla para voltar ao Menu>");
+            Console.ReadKey();
+            Menu();
         }
 
+
+        public static void SalarioFuncionario()
+        {
+            Console.Clear();
+            Linha(col);
+            Console.Write("Digite o salario atual do funcionario : ");
+            salario = Convert.ToSingle(Console.ReadLine());
+            Linha(col);
+            Console.WriteLine("Salario Atual Cadastrado...");
+            preenchidoSalario=true;
+            Console.WriteLine();
+            Console.Write("<Precione uma tecla para voltar ao Menu>");
+            Console.ReadKey();
+            Menu(); 
+        }
+
+        public static void TempoEmpresa()
+        {
+            Console.Clear();
+            Linha(col);
+            Console.Write("Digite tempo de empresa em Anos: ");
+            anos = Convert.ToSingle(Console.ReadLine());
+            Linha(col);
+            Console.WriteLine("Tempo de empresa cadastrado...");
+            preenchidoAnos = true;
+            Console.WriteLine();
+            Console.Write("<Precione uma tecla para voltar ao Menu>");
+            Console.ReadKey();
+            Menu();
+        }
+
+
+        public static void Calculo()
+        {
+            Console.Clear();
+            Linha(col);
+            var ptbr = CultureInfo.CreateSpecificCulture("pt-br");              
+            if (preenchidoNome==false || preenchidoSalario==false || preenchidoAnos==false)
+            {
+                Console.WriteLine("Voce ainda não cadastros todos os dados, volte ao menu e finalize");
+                Console.WriteLine("<Precione uma tecla para voltar ao Menu>");
+                Console.ReadKey(); 
+                Menu();
+            }
+            Console.WriteLine($"Nome : {nome}");
+            Console.WriteLine($"Salario atual: {salario.ToString("C",ptbr)}");
+            Console.WriteLine($"Tempo de empresa : {anos} anos");
+            Linha(col);
+
+            if(anos <3) { novosalario = salario * aumento3; }
+            if (anos >= 3 && anos <10) { novosalario = salario * aumenta3a10; }
+            if (anos >= 10) { novosalario = salario * aumenta10eacima; }
+            Console.WriteLine($"O novo salario : {novosalario.ToString("C", ptbr)}");
+            Linha(col);
+            Console.WriteLine();
+            Console.Write("<Precione uma tecla para voltar ao Menu>");
+            Console.ReadKey();
+            Menu();
+        }
+
+        public static void ZeraPreenchimento()
+        {
+
+            preenchidoNome = false;
+            preenchidoSalario = false;
+            preenchidoAnos = false;
+            nome = "";
+            salario = 0.00f;
+            anos = 0.00f;
+            Menu();
+            
+        }
     }
+
+
 }
